910d95bc25bef36d3b88604523765c92d9293122
krisht <krisht@cooper.edu>
2016-04-30 03:41:33 -0400

    Started modifying code in order to make an efficient sorting depending on file passed

--- MasterSort.cpp
+++ MasterSort.cpp
@@ -15,6 +15,7 @@
 
 using namespace std;
 
+
 // A simple class; each object holds one string
 class Data {
 public:
@@ -102,6 +103,75 @@ int main() {
 // You may add global variables, functions, and/or
 // class defintions here if you wish.
 
+// QuickSort T1, T2, Radix Sort T3, Insertion Sort T4
+
+
+//Public Variables Declaration
+list<Data *>::iterator startIterator; 
+list<Data *>::iterator endIterator;
+unsigned long long testArray[10];
+
+void insertionSort(int size){
+
+}
+
+void radixSort(int size){
+  
+}
+
 void sortDataList(list<Data *> &l) {
   // Fill in this function
+  int fileType; 
+  startIterator = l.begin();
+  endIterator = l.end();
+  endIterator--;
+  int listSize = l.size();
+
+  //Code below finds the type of file that is being handled & sorted
+
+  if(listSize > 500000){
+    if((*startIterator)->data.size()/sizeof(char) <= 7)
+      fileType = 3;
+    else{
+      testArray[0] = strtoull((startIterator*)->data.substr(0,8).c_str(),0,10);
+      startIterator++;
+      testArray[1] = strtoull((startIterator*)->data.substr(0,8).c_str(),0,10);
+      if(testArray[0] == testArray[1])
+        fileType = 4;
+      else fileType = 2;
+    }
+  }
+  else fileType = 1; 
+
+  starIterator = l.begin(); // Resets startIterator
+
+
+  //Arranges 
+  if(fileType == 3){
+
+  }
+  else if(type != 4){
+
+  }
+  else{
+
+  }
+
+  //Switch statement that sorts the list
+  switch(fileType){
+    case 1:
+      //QuickSort
+      break;
+    case 2: 
+      // QuickSort
+      break; 
+    case 3: 
+      //RadixSort
+      break; 
+    case 4: 
+      //Insertion Sort
+      break; 
+  };
+
+  cout << "Sorting complete!" << endl;
 }
