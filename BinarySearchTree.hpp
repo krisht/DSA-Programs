6efdda863cbede386bf2d04fd6a4f8094ab1d1b6
Krishna Thiyagarajan <krisht@cooper.edu>
2016-07-04 18:08:58 -0400

    Final touches to BST

--- BinarySearchTree.hpp
+++ BinarySearchTree.hpp
@@ -5,43 +5,51 @@
 using namespace std; 
 
 template<typename T>
-class Node{
+class BSTNode{
 private:
 	T data;
-	Node<T>* left; 
-	Node<T>* right;
+	BSTNode<T>* left; 
+	BSTNode<T>* right;
+	friend std::ostream& operator<<(std::ostream &strm, const BSTNode* a){
+		if(a == NULL)
+			return strm << "NULL"; 
+		return strm << "BSTNode(Data: "<< a->data << ")"; 
+	}
 public:
-	Node(T data); 
+	BSTNode(T data); 
 	void setData(T data);
-	void setRight(Node<T>* node); 
-	void setLeft(Node<T>* node); 
+	void setRight(BSTNode<T>* node); 
+	void setLeft(BSTNode<T>* node); 
 	T getData(); 
-	Node<T>* getLeft(); 
-	Node<T>* getRight(); 
+	BSTNode<T>* getLeft(); 
+	BSTNode<T>* getRight(); 
 }; 
 
 template<typename T>
 class BinarySearchTree{
 private: 
-	Node<T>* root;
+	BSTNode<T>* root;
 	int numNodes; 
-	void clearTree(Node<T>* t); 
-	Node<T>* duplicateNode(Node<T>* t); 
-	void printAll(Node<T>* t); 
+	void clearTree(BSTNode<T>* t); 
+	BSTNode<T>* duplicateNode(BSTNode<T>* t); 
+	void printAll(BSTNode<T>* t); 
+	friend std::ostream& operator<<(std::ostream &strm, const BinarySearchTree* a){
+		return strm << "BST(Root Node: "<< a->root << ", # of Nodes: " << a->numNodes << ")"; 
+	}
 public:
 	BinarySearchTree();
 	~BinarySearchTree();
-	Node<T>* getRoot();
-	void setRoot(Node<T>* newRoot); 
+	BSTNode<T>* getRoot();
+	void setRoot(BSTNode<T>* newRoot); 
 	bool isEmpty();
-	Node<T>* searchTree(T data); 
-	bool insert(Node<T>* newNode); 
+	BSTNode<T>* searchTree(T data); 
+	bool insert(BSTNode<T>* newNode); 
 	bool insert(T data);
 	bool deleteNode(T data); 
-	Node<T>* findMin();
-	Node<T>* findMax(); 
+	BSTNode<T>* findMin();
+	BSTNode<T>* findMax(); 
 	int getNumNodes(); 
-	void printOne(Node<T>* t); 
+	void printOne(BSTNode<T>* t); 
 	void printTree(); 
 
 }; 
--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,22 @@
 #include <cstdlib> 
 #include <ctime> 
 #include <iostream>
-#include <string.h>
 #include "BinarySearchTree.hpp"
 
 using namespace std; 
 
 int main(){
-	BinarySearchTree<float>* theTree; 
-	Node<float>* newNode; 
+	BinarySearchTree<float>* theTree;
 
 	theTree = new BinarySearchTree<float>(); 
 
 	srand((unsigned)time(0)); 
 	for(int i = 0; i < 1000; i++){
     	float randNum = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) ; 
-    	theTree->insert(new Node<float>(randNum));
-	}	
+    	theTree->insert(new BSTNode<float>(randNum));
+	}
 
-	theTree->printTree();  
+	cout << theTree << endl; 
 
     return 0;
 
